22b1c61bc31cef8460dfebf1f35b17c3d8ac6f6e
Christopher Lye <christopher.lye@jublia.com>
2022-09-24 23:10:03 +0800

    feat: day 1 done

---
+++ day1-band-name-generator/README.md
@@ -0,0 +1,3 @@
+# Day 1
+A simple python script that makes use of the input and print functions, as well as string concatenation.    
+Simply run `python main.py` to start generating your sinkie band name.
---
+++ day1-band-name-generator/main.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print("Welcome to the band name generator!")
+#2. Ask the user for the city that they grew up in.
+town = input("Which part of Singapore do you live in?\n")
+#3. Ask the user for the name of a pet.
+pet = input("What is the name of a pet you have had?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " +town+ " " + pet)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
