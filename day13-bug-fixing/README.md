f29c6b61d82f694b60b0285020348512cb5e6130
Christopher Lye <christopher.zijian@gmail.com>
2022-10-06 18:35:44 +0800

    feat: day 13 done

---
+++ day13-bug-fixing/README.md
@@ -0,0 +1,3 @@
+# Day 13
+A lesson on bug fixing, using debuggers and print statements, and how to find information on error messages.
+No project this week.
---
+++ day13-bug-fixing/main.py
@@ -0,0 +1,20 @@
+### Original ###
+# for number in range(1, 101):
+#   if number % 3 == 0 or number % 5 == 0:
+#     print("FizzBuzz")
+#   if number % 3 == 0:
+#     print("Fizz")
+#   if number % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print([number])
+### Fixed ###
+for number in range(1, 101):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
