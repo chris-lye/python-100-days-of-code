46b3c21f576e68b2e4835359c8d1333b29dc1030
Christopher Lye <christopher.zijian@gmail.com>
2022-10-12 00:42:15 +0800

    feat: day 18 done

---
+++ day18-turtle-dot-painting/README.md
@@ -0,0 +1,3 @@
+# Day 18
+A script that starts art generator made with the turtle library.
+Simply run `python main.py` to start the application.
---
+++ day18-turtle-dot-painting/main.py
@@ -0,0 +1,37 @@
+# import colorgram
+
+# rgb_colors = []
+# colors = colorgram.extract('./image.jpg', 30)
+# for color in colors:
+#     rgb_colors.append(color. )
+
+# print(rgb_colors)
+
+import turtle as t
+import random
+t.colormode(255)
+turtly = t.Turtle()
+screen = t.Screen()
+color_list = [( 202,  164,  110), ( 149,  75,  50), ( 222,  201,  136), ( 53,  93,  123), ( 170,  154,  41), ( 138,  31,  20),  ( 134,  163,  184),  ( 197,  92,  73),  ( 47,  121,  86),  ( 73,  43,  35),  ( 145,  178,  149),  ( 14,  98,  70),  ( 232,  176,  165),  ( 160,  142,  158),  ( 54,  45,  50),  ( 101,  75,  77),  ( 183,  205,  171),  ( 36,  60,  74),  ( 19,  86,  89),  ( 82,  148,  129),  ( 147,  17,  19),  ( 27,  68,  102),  ( 12,  70,  64), ( 107,  127,  153), ( 176,  192,  208), ( 168,  99,  102)]
+
+turtly.speed(0)
+turtly.penup()
+turtly.hideturtle()
+
+turtly.setheading(225)
+turtly.forward(300)
+turtly.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    turtly.dot(20, random.choice(color_list))
+    turtly.forward(50)
+    
+    if dot_count % 10 == 0:
+        turtly.setheading(90)
+        turtly.forward(50)
+        turtly.setheading(180)
+        turtly.forward(500)
+        turtly.setheading(0)
+        
+screen.exitonclick()
