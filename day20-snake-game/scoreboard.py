97c6477305a43f967b076ca9f9310cf59fa422ae
Christopher Lye <christopher.zijian@gmail.com>
2022-10-17 23:34:58 +0800

    add highscore storing in txt file for day 20 snake game

--- day20-snake-game/scoreboard.py
+++ day20-snake-game/scoreboard.py
@@ -7,12 +7,20 @@ class Scoreboard(Turtle):
     def __init__(self) -> None:
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.get_highscore()
         self.color("white")
         self.penup()
         self.goto(0, 270)    
         self.hideturtle()
         self.update_scoreboard()
+        
+    def get_highscore(self):
+        with open("highscore.txt", 'r') as file:
+            return int(file.read())
+        
+    def set_highscore(self):
+        with open("highscore.txt", "w") as file:
+            file.write(str(self.high_score))
         
     def update_scoreboard(self):
         self.write(f"Score: {self.score} Highscore: {self.high_score}", align=ALIGNMENT, font=FONT)
@@ -21,6 +29,7 @@ class Scoreboard(Turtle):
         self.clear()
         if self.score > self.high_score:
             self.high_score = self.score
+        self.set_highscore()
         self.score =  0
         self.update_scoreboard()
         
