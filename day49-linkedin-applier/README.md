4812c33bdb1c58eff8f1195951fdcb889715ec8c
Christopher Lye <christopher.zijian@gmail.com>
2022-12-05 21:38:36 +0800

    feat: day 49 done

---
+++ day49-linkedin-applier/README.md
@@ -0,0 +1,3 @@
+# Day 49        
+A script that uses Selenium to apply for jobs. Currently only applies for the top-most job on Linkedin. 
+Simply run `python main.py` to start.  
---
+++ day49-linkedin-applier/main.py
@@ -0,0 +1,31 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+URL = 'https://www.linkedin.com/jobs/search/?currentJobId=3312205122&f_AL=true&f_JT=I&geoId=102454443&keywords=full%20stack&location=Singapore&refresh=true&sortBy=R'
+
+chrome_driver_path = r'C:\Users\ChrisLye\Documents\DevDependencies\chromedriver_win32\chromedriver'
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+driver.get(URL)
+time.sleep(1)
+# click on sign in
+sign_in_button = driver.find_element(By.LINK_TEXT, ("Sign in"))
+sign_in_button.click()
+# key in user and pass
+username_input = driver.find_element(By.ID, "username")
+username_input.send_keys(username)
+password_input = driver.find_element(By.ID, "password")
+password_input.send_keys(password)
+# click on sign in
+sign_in_button = driver.find_element(By.XPATH,  "//*[contains(@aria-label, 'Sign in')]")
+sign_in_button.click()
+# save the job
+save_button = driver.find_element(By.CLASS_NAME, 'jobs-save-button')
+save_button.click()
+
+time.sleep(100)
